18b4d7f69d1a13edefe59e41c97d1a063b52033d
Dennis Fricke <dennis.fricke@ninjaneers.de>
2020-01-22 10:18:25 +0100

    fix(pipeline): fix update_chart

--- .ci/update_chart.sh
+++ .ci/update_chart.sh
@@ -4,7 +4,7 @@ CONTENT=$(base64 $NAME-$VERSION.tgz | tr -d \\n)
 
 echo "Upload chart $NAME"
 
-curl -X PUT -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/json" \
+curl -X PUT -H "Authorization: token $GH_TOKEN" -H "Content-Type: application/json" \
   "https://api.github.com/repos/$TEAM/$REPOSITORY/contents/$NAME-$VERSION.tgz" \
   -d "{
         \"message\":\"Chart update\",
